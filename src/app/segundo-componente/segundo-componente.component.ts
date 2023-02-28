f3027e1cc98bf474c7a6bc19fdd3469958b9956d
Matheus Victor <matheusvictor.dev@gmail.com>
2023-02-28 14:51:20 -0300

    Adiciona o segundo componente, que mostra nome e data de nascimento em tela.

---
+++ src/app/segundo-componente/segundo-componente.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-segundo-componente',
+  templateUrl: './segundo-componente.component.html',
+  styleUrls: ['./segundo-componente.component.css']
+})
+export class SegundoComponenteComponent {
+  nome = "Matheus";
+  dataNascimento = "1995-06-23";
+}
+  
